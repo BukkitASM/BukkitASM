ec2c02675071e74cd556c05e9d91fb7fb387dda4
kingdevnl <jasper@kingdev.nl>
2017-10-02 22:48:02 +0200

    Remove not needed imports

--- src/main/java/com/bukkitasm/launch/transformer/BukkitMethodVisitor.java
+++ src/main/java/com/bukkitasm/launch/transformer/BukkitMethodVisitor.java
@@ -1,7 +1,5 @@
 package com.bukkitasm.launch.transformer;
 
-import net.minecraft.server.v1_12_R1.CommandOp;
-import org.bukkit.Bukkit;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -13,7 +11,6 @@ public class BukkitMethodVisitor extends MethodVisitor implements Opcodes {
     private String className;
     private String methodName;
 
-
     public BukkitMethodVisitor(String className, MethodVisitor methodVisitor, String methodName) {
         super(ASM5, methodVisitor);
         this.className = className;
@@ -23,25 +20,21 @@ public class BukkitMethodVisitor extends MethodVisitor implements Opcodes {
     @Override
     public void visitLdcInsn(Object cst) {
         Object returnObject = cst;
-        if(cst instanceof  String) {
+        if (cst instanceof String) {
             String string = String.valueOf(cst);
             //System.out.println(className + " String: " +  string);
 
-            if(string.equals("multiplayer.player.joined")) {
+            if (string.equals("multiplayer.player.joined")) {
                 returnObject = String.valueOf("[BukkitASM] A player joined the Game!");
                 System.out.println("[BukkitASM] Join message changed!");
             }
 
-
-            if(className.contains("CommandOp")) {
-                if(string.equals("op")) {
+            if (className.contains("CommandOp")) {
+                if (string.equals("op")) {
                     returnObject = String.valueOf("adm");
                     System.out.println("[BukkitASM] OpCommand changes to adm !");
                 }
             }
-
-
-
         }
 
         super.visitLdcInsn(returnObject);
